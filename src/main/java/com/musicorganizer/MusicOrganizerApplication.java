6bd17f764297444c2cb5f9b83885d15dbb7d7755
Unknown <b.peterson@kunzleigh.com>
2017-01-04 14:02:26 -0500

    Initial commit

---
+++ src/main/java/com/musicorganizer/MusicOrganizerApplication.java
@@ -0,0 +1,12 @@
+package com.musicorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicOrganizerApplication.class, args);
+	}
+}
